857f36b1880edc674ab04531a4ba479094263704
Swapnanil Dhol <swapnanildhol@gmail.com>
2020-07-18 16:37:30 +0530

    Initial Commit

---
+++ AppleMusicGradient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMusicGradient
+//
+//  Created by Swapnanil Dhol on 7/18/20.
+//  Copyright © 2020 Swapnanil Dhol. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
